fab887bd9899cbf6390d45fba258f7b5b027a7ab
vayne <miguel.osorio19@yahoo.com>
2022-02-04 14:46:50 -0600

    testing connection to database

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const db = require('./db/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+});
+
