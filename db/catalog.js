a4f776d099f8cf4c72cda507039e73ad17b34200
vayne <miguel.osorio19@yahoo.com>
2022-02-04 18:55:11 -0600

    testing chaining inquirer prompts through functions

--- db/catalog.js
+++ db/catalog.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 // const mysql = require('mysql2');
 const cTable = require('console.table');
+const { message } = require('statuses');
 
 function catalog() {
     // inquirer
@@ -16,6 +17,27 @@ function catalog() {
             "add a role",
             "add an employee",
             "update an employee role"
+        ]
+    })
+    .then((choice) =>{
+       switch (choice.action) {
+        case "view all departments":
+            viewAlldepartments();
+            break;
+        }
+    })
+}
+
+function viewAlldepartments (){
+    inquirer.prompt({
+        name: "department",
+        type: "list",
+        message: 'Which department would you like to choose?',
+        choices: [
+            'Administration',
+            'Engineering',
+            'Accounting',
+            'Sales'
         ]
     })
 }
